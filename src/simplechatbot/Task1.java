9c0fa390e73b3ed90cf6fc16a4657784006d6f2b
mobile <ForthA>
2021-12-08 19:45:37 +0500

    первое задание

--- src/simplechatbot/Task1.java
+++ src/simplechatbot/Task1.java
@@ -2,6 +2,7 @@ package simplechatbot;
 
 public class Task1 {
     public static void main(String[] args) {
-        System.out.println("Hello there!");
+        System.out.println("I was created in 2021.");
+        System.out.println("Hello! My name is robot.");
     }
 }
